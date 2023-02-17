9c3eca572c381e94a101b1f6826ce6025e128252
zuhaz <zuhaztalal30@gmail.com>
2023-02-17 18:23:00 +0500

    GG

--- Events/Client/viperHandler.js
+++ Events/Client/viperHandler.js
@@ -5,47 +5,43 @@ const moment = require("moment/moment");
 module.exports = {
   name: "ready",
   execute: async (client) => {
-    const guilds = client.guilds.cache;
-    for (const id of guilds) {
-      try {
-        const setupData = await setupDB.findOne({ GuildID: id });
+    cron.schedule("0 */6 * * *", async function () {
+      const guilds = client.guilds.cache;
+      for (const id of guilds) {
+        try {
+          const setupData = await setupDB.findOne({ GuildID: id });
 
-        if (!setupData) return;
+          if (!setupData) return;
 
-        const jtcchannels = setupData.JTCInfo.map((element) => {
-          return element.channel;
-        });
+          const jtcchannels = setupData.JTCInfo.map((element) => {
+            return element.channel;
+          });
 
-        if (jtcchannels && jtcchannels.length !== 0) {
-          const joinTime = moment(jtcchannels.lastJoinedTime);
-          const currentTime = moment();
-          const diffInMinutes = currentTime.diff(joinTime, "seconds");
-          if (diffInMinutes <= 10) {
-            const size = Object.keys(setupData.JTCInfo).length;
-            for (let i = 0; i < size; i++) {
-              const channel = await client.channels.fetch(jtcchannels[i]);
-              if (channel && channel.members.size === 0) {
-                await channel.delete();
-                await setupDB.updateOne(
-                  {
-                    GuildID: id,
-                  },
-                  { $pull: { JTCInfo: { owner: jtcchannels.owner } } }
-                );
+          if (jtcchannels && jtcchannels.length !== 0) {
+            const joinTime = moment(jtcchannels.lastJoinedTime);
+            const currentTime = moment();
+            const diffInMinutes = currentTime.diff(joinTime, "minutes");
+            if (diffInMinutes <= 10) {
+              const size = Object.keys(setupData.JTCInfo).length;
+              for (let i = 0; i < size; i++) {
+                const channel = await client.channels.fetch(jtcchannels[i]);
+                if (channel && channel.members.size === 0) {
+                  await channel.delete();
+                  await setupDB.updateOne(
+                    {
+                      GuildID: id,
+                    },
+                    { $pull: { JTCInfo: { owner: jtcchannels.owner } } }
+                  );
+                }
               }
             }
           }
+        } catch (error) {
+          console.log("Error:", error);
+          continue;
         }
-      } catch (error) {
-        console.log("Error:", error);
-        continue;
       }
-    }
-
-    // cron.schedule("*/1 * * * *", function () {
-    //   channel.send({ content: "testing" });
-    // });
-
-    // console.log(`The bot is connected to ${guilds.size} guild(s).`);
+    });
   },
 };
