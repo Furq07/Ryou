f31a34d506d61a09c2c534fe0a04c308c29cd858
zuhaz <zuhaztalal30@gmail.com>
2023-02-17 17:35:28 +0500

    sdsd

--- Events/Client/viperHandler.js
+++ Events/Client/viperHandler.js
@@ -1,59 +1,51 @@
 const cron = require("node-cron");
 const setupDB = require("../../src/models/setupDB");
 const moment = require("moment/moment");
+
 module.exports = {
   name: "ready",
-  async execute(client) {
-    client.channels.fetch("1056559754513760276").then(async (channel) => {
-      channel.send({ content: "testing" });
-      client.guilds.cache.forEach(async (guild) => {
-        try {
-          var size = Object.keys(setupData.JTCInfo).length;
-          console.log(size);
-          // for (let i = 0; i < size; i++) {
-          //   let setupData = await setupDB.findOne({ GuildID: guild.id });
-          //   console.log(guild.id);
-          //   const jtcchannels = setupData.JTCInfo.find((element) => {
-          //     if (element.length === 0) return;
-          //     return element.length !== 0 ?? element;
-          //   });
-          //   console.log(jtcchannels);
-          //   if (jtcchannels) {
-          //     const joinTime = moment(jtcchannels.lastJoinedTime);
-          //     console.log(jtcchannels.lastJoinedTime);
-          //     const currentTime = moment();
-          //     const diffInMinutes = currentTime.diff(joinTime, "seconds");
-          //     if (diffInMinutes <= 10) {
-          //       console.log("Channel joined in the last 10 minutes");
-          //     } else {
-          //       console.log("Channel not joined in the last 10 minutes");
-          //       await client.channels
-          //         .fetch(jtcchannels.channel)
-          //         .then(async (channel) => {
-          //           await channel.delete();
-          //           await setupDB.updateOne(
-          //             {
-          //               GuildID: guild.id,
-          //             },
-          //             { $pull: { JTCInfo: { owner: jtcchannels.owner } } }
-          //           );
-          //         })
-          //         .catch(() => {
-          //           return;
-          //         });
-          //     }
-          //   }
-          // }
-        } catch {
-          return;
-        }
+  execute: async (client) => {
+    const guilds = client.guilds.cache;
+    for (const id of guilds) {
+      try {
+        const setupData = await setupDB.findOne({ GuildID: id });
 
-        // If the channel has been inactive for more than 10 minutes, delete it and its document
-        // cron.schedule("*/1 * * * *", function () {
-        //   channel.send({ content: "testing" });
-        // });
-        // console.log(`The bot is connected to the guild with ID ${guild.id}`);
-      });
-    });
+        if (!setupData) return;
+
+        const jtcchannels = setupData.JTCInfo.map((element) => {
+          return element.channel;
+        });
+
+        if (jtcchannels && jtcchannels.length !== 0) {
+          const joinTime = moment(jtcchannels.lastJoinedTime);
+          const currentTime = moment();
+          const diffInMinutes = currentTime.diff(joinTime, "seconds");
+          if (diffInMinutes <= 10) {
+            const size = Object.keys(setupData.JTCInfo).length;
+            for (let i = 0; i < size; i++) {
+              const channel = await client.channels.fetch(jtcchannels[i]);
+              if (channel && channel.members.size === 0) {
+                await channel.delete();
+                await setupDB.updateOne(
+                  {
+                    GuildID: id,
+                  },
+                  { $pull: { JTCInfo: { owner: jtcchannels.owner } } }
+                );
+              }
+            }
+          }
+        }
+      } catch (error) {
+        console.log("Error:", error);
+        continue;
+      }
+    }
+
+    // cron.schedule("*/1 * * * *", function () {
+    //   channel.send({ content: "testing" });
+    // });
+
+    // console.log(`The bot is connected to ${guilds.size} guild(s).`);
   },
 };
