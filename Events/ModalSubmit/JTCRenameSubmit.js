9fb70687704e412e126738309b601b64ee3a69b1
zuhaz <zuhaztalal30@gmail.com>
2023-02-25 17:20:58 +0500

    a

--- Events/ModalSubmit/JTCRenameSubmit.js
+++ Events/ModalSubmit/JTCRenameSubmit.js
@@ -115,7 +115,6 @@ module.exports = {
               .setThumbnail(interaction.guild.iconURL({ dynamic: true })),
           ],
           components: [],
-          embeds: [],
           ephemeral: true,
         });
 
